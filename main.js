5532d7416ad702ca083638ce521fe6705c751404
Michael Caridad <caridadmichael@gmail.com>
2022-12-21 20:49:44 -0400

    Added images and more styling

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ const mobile_menu = document.querySelector('.mobile-menu');
 hamburger.addEventListener('click', ()=>{
     hamburger_icon.innerText = hamburger_icon.innerText === 'menu' ? 'close' : 'menu';
 
-    mobile_menu.classList.toggle('is-open');
+    mobile_menu.classList.toggle('mobile-menu');
 })
