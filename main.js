c26d3dc131001a3d530367ec209d637c1797ba38
Michael Caridad <caridadmichael@gmail.com>
2023-01-02 17:37:34 -0400

    add alot of things

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 const hamburger = document.querySelector('.hamburger');
 const hamburger_icon = hamburger.querySelector('span');
 const mobile_menu = document.querySelector('.mobile-menu');
+const anchor_tags = document.querySelectorAll('.anchor')
+
+anchor_tags.forEach(anchor => {
+   anchor.addEventListener('click', function handleClick(event) {
+        hamburger_icon.innerText = hamburger_icon.innerText === 'menu' ? 'close' : 'menu';
+         mobile_menu.classList.toggle('mobile-menu');
+  
+     
+    });
+  });
+
+
+
+
 
 hamburger.addEventListener('click', ()=>{
     hamburger_icon.innerText = hamburger_icon.innerText === 'menu' ? 'close' : 'menu';
