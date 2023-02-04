c24a11f7616650cd6b4ffc248caffd12eda1749f
Michael Caridad <caridadmichael@gmail.com>
2023-02-03 23:18:10 -0400

    a lot of colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-      'dark-blue': '#0a192f',
-      'medium-blue' : '#8892b0',
-      'light-blue' : '#64ffda',
-      'special-blue' :'#64ffDA'
+      'dark-blue': '#264653',
+      'medium-blue' : '#e9c46a',
+      'light-blue' : '#e9c46a',
+      'other-blue':'#f4a261'
+      
     },
   },
   },
