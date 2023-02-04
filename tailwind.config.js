acbbd85bbcc34a1fb4dcd4d705f46faccbed8867
Michael Caridad <caridadmichael@gmail.com>
2023-02-03 22:52:49 -0400

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+      'nunito': 'Nunito Sans',
+      'gaya':'Gayathri'
+    },
+  },
   },
   plugins: [],
 }
