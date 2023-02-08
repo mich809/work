4de3f811c0d14b080660d29f1b22d684cae5f746
Michael Caridad <caridadmichael@gmail.com>
2023-02-08 14:41:58 -0400

    increase font size and move scripts to js file

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-
+const btn = document.querySelector("button.menu-button");
+const menu = document.querySelector(".mobile-menu");
+const listOfLinks = [...document.links];
+      btn.addEventListener("click", () => {
+        menu.classList.toggle("hidden");
+      });
+      listOfLinks.forEach((element) => {
+        element.addEventListener("click", () => {
+          menu.classList.toggle("hidden");
+        });
+      });
 
 
 
