0128af5ae81d09031eba84514a2676dde17fd066
Michael Caridad <caridadmichael@gmail.com>
2023-02-17 12:49:44 -0400

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./**/**/*.{html,js}"],
   theme: {
     extend: {
+      margin: {
+        bottom : '0px 0px 3rem 0px'
+      },
       colors: {
       'dark-blue': '#264653',
       'medium-blue' : '#e9c46a',
